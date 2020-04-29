7a47a34be4f6e5f10c47b69f2c31896971758307
TrinaT <ttanda@ualberta.ca>
2020-04-29 11:11:18 -0600

    added includes folder to store reusable information

---
+++ includes/datatypes.php
@@ -0,0 +1,27 @@
+<?php
+//String. Concatenation is done with periods, and + is reserved for addition
+$myString = 'Hello World!';
+//Integer.
+$myInteger =34;
+//Float/Double
+$myFloat = 3.14;
+//Boolean
+$myBoolTrue = TRUE;
+$myBoolFalse = false;
+//Null
+$myNull = Null;
+//Object
+$myObject = new stdClass();
+//Array
+$myArray = ['First item', 2, 'third item'];
+$myOtherArray = array(
+    $myString,
+    $myInteger,
+    $myFloat,
+    $myBoolTrue,
+    $myBoolFalse,
+    $myNull,
+    $myObject,
+    $myArray
+);
+?>
