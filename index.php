5f79ae7cdc15e60b4cfb747406898445977d4d96
TrinaT <ttanda@ualberta.ca>
2020-04-29 13:55:40 -0600

    added session

--- index.php
+++ index.php
@@ -2,23 +2,25 @@
 include './includes/datatypes.php';
 ?><DOCTYPE html>
 
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>First PHP</title>
-</head>
-<body>
-    <h1>PHP Example</h1>
-    <pre>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>First PHP</title>
+    </head>
+
+    <body>
+        <h1>PHP Example</h1>
+        <pre>
     <?php var_dump($myOtherArray);
     ?>
     <?php
-    $myVar ="Hello, World!";
+    $myVar = "Hello, World!";
     $name = 'Trina';
-    $greetig ="Hi, my name is: $name ";
-    $lang ='PHP';
-    $greeting2= "$name here . I think I get this basic $lang example.";
+    $greetig = "Hi, my name is: $name ";
+    $lang = 'PHP';
+    $greeting2 = "$name here . I think I get this basic $lang example.";
     echo $myVar;
     echo $greeting2;
     ?>
