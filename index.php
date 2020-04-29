d07b4e94b171411c330ab99e9a216d94a30ed33f
TrinaT <ttanda@ualberta.ca>
2020-04-29 09:24:44 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP example</title>
+</head>
+<body>
+    <h1>PHP Example</h1>
+    <?php
+    $myVar ="Hello, World!";
+    $name = 'Trina';
+    $greeting ="Hi, my name is: $name ";
+    $lang ='PHP';
+    $greeting2= "$name here . I think I get this basic $lang example.";
+    echo $myVar;
+    echo $greeting2;
+    ?>
+</body>
+</html>
